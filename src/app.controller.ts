014f45489dc2f978944024c3fc2312a19c764865
neema-deshwal <neema@geekyants.com>
2026-06-17 21:50:45 +0530

    feat: create health

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,17 +3,46 @@ import { Public } from './auth/public.decorator';
 import express from 'express';
 import { InjectQueue } from '@nestjs/bullmq';
 import { Queue } from 'bullmq';
+import { DatabaseService } from './database/database.service';
+import { RedisService } from './redis/redis.service';
+import { timestamp } from 'rxjs';
 
 @Controller()
 export class AppController {
 constructor(
     @InjectQueue('expiry') private expiryQueue: Queue,
     @InjectQueue('trending') private trendingQueue: Queue,
-  ) {}
+     private db: DatabaseService,
+    private redis: RedisService,
+  ) {
+    
+  }
   @Public()
   @Get('health')
-  health() {
-    return { status: 'ok' };
+async  health() {
+   let database='ok';
+   let redisStatus='ok';
+
+   try{
+        await this.db.$queryRaw`SELECT 1`;
+   }
+   catch{
+    database='error';
+   }
+   try{
+      await this.redis.set('health', 'ok', 10);
+    
+   }
+   catch{
+    redisStatus='error';
+   }
+   return {
+    status:database==='ok'&&redisStatus==='ok'?'ok':'degraded',
+    database,
+    redis:redisStatus,
+    timestamp:new Date()
+
+   }
   }
 
   @Get('me')
